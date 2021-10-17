decb9aa50b973703f891d8aa5069c7ad975f8c62
Vladislava Markova <vladislavam1996@gmail.com>
2021-10-17 11:58:38 +0300

    prototypes logic, function call, apply and bind

--- exercises161021part2.js
+++ exercises161021part2.js
@@ -37,38 +37,104 @@
 // var f_person_data = f_person.getData();
 // console.log(f_person_data);
 
-var objPrototype = {
-    getData: function() {
-        return 100;
-    }
-};
+// var objPrototype = {
+//     getData: function() {
+//         return 100;
+//     }
+// };
 
-var t_obj = Object.create(objPrototype, {
-    a: {
-        value: 999,
-        writable: false
-    },
-    b: {
-        configurable: false, 
-        writable: false, 
-        value: function() {
-            console.log('Hello World!');
-        }
-    }
-})
+// var t_obj = Object.create(objPrototype, {
+//     a: {
+//         value: 999,
+//         writable: false
+//     },
+//     b: {
+//         configurable: false, 
+//         writable: false, 
+//         value: function() {
+//             console.log('Hello World!');
+//         }
+//     }
+// })
 
-console.log(t_obj.getData()); // 100
-console.log(t_obj.a); // 999
-t_obj.b(); // 'Hello World!'
+// console.log(t_obj.getData()); // 100
+// console.log(t_obj.a); // 999
+// t_obj.b(); // 'Hello World!'
 
-t_obj.a = 777;
+// t_obj.a = 777;
 
-console.log(t_obj.a); // 999
+// console.log(t_obj.a); // 999
 
-t_obj.b = function() {
-    console.log('Hello again!');
-}
+// t_obj.b = function() {
+//     console.log('Hello again!');
+// }
 
-t_obj.b(); // Hello World! 
+// t_obj.b(); // Hello World! 
 
+// function Person(name, age) {
+//     this.name = name,
+//     this.age = age;
+// }
 
+// Person.prototype.city = 'Kyustendil';
+// Person.prototype.getData = function(param1, param2, param3) {
+//     console.log(param1, param2);
+//     return 'name: ' + this.name + ' age: ' + this.age;
+// }
+
+// var f_person = new Person('Ivan', 28);
+
+// console.log(f_person);
+// console.log(f_person.city);
+// personData = f_person.getData();
+// console.log(personData);
+
+// var fn = Person.prototype.getData;
+
+// f_person.fn = Person.prototype.getData;
+// p_data = f_person.fn(); // undefined undefined
+
+// console.log(p_data) // name: 'Ivan' age: 28
+
+// var test_obj = {
+//     name: 'testName'
+// };
+
+// test_obj.fn = Person.prototype.getData;
+
+// test_result = test_obj.fn();
+
+// console.log(test_result); // name: testName age: undefined 
+
+// f_person.city = 'Plovdov';
+
+// console.log(f_person.city); // Plovdiv
+
+// var fResultString = fn.call(
+//     { name: "tName, age: 34" },
+//     1, 
+//     2
+// );
+
+// var sResultString = fn.apply(
+//     { name: 'tName', age: 34 },
+//     [1 ,2]
+// );
+
+// var bFunction = fn.bind(
+//     { name: 'tName', age: 34 },
+//     1,
+//     2
+// );
+
+// console.log(fResultString);
+// console.log(sResultString);
+
+// var bFunctionResult = bFunction(32, 33, 34);
+// console.log(bFunctionResult);
+
+// var s_person = new Person('Anna', 42);
+// s_person.city = 'Burgas';
+
+// console.log(s_person.city); // Kyustendil 
+// console.log(f_person.city); // Burgas 
